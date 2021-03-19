a08093c702573a0d31899f4291759a877d9090f6
yunqingjia <yjia16@outlook.com>
2021-03-19 16:02:29 -0400

    completed problem 1

--- PS4/proj4_code/quantization_student.py
+++ PS4/proj4_code/quantization_student.py
@@ -21,15 +21,29 @@ def quantizeRGB(origImg: np.ndarray, k: int) -> np.ndarray:
         - quantizedImg: Quantized image with shape H x W x 3 and dtype "uint8"
         - clusterCenterColors: k x 3 vector of hue centers
     """
-    quantizedImg = np.zeros_like(origImg)
+
     ######################################################################################
     ## TODO: YOUR CODE GOES HERE                                                        ##
     ######################################################################################
 
+    h, w = origImg.shape[0], origImg.shape[1]
+    flat_img = origImg.reshape(h*w, 3)
+    quantizedImg = np.zeros_like(flat_img)
+
+    km = KMeans(n_clusters=k, random_state=101)
+    km.fit_predict(flat_img)
+    clusterCenterColors = km.cluster_centers_
+    labels = km.labels_
+    for i in range(len(labels)):
+        quantizedImg[i] = clusterCenterColors[labels[i]]
+
+    quantizedImg = np.floor(quantizedImg.reshape(h, w, 3)).astype(int)
+
+    return quantizedImg, clusterCenterColors
+
     ######################################################################################
     ## YOUR CODE ENDS HERE                                                              ##
     ######################################################################################
-    raise NotImplementedError
 
 
 def quantizeHSV(origImg: np.ndarray, k: int) -> np.ndarray:
@@ -48,15 +62,35 @@ def quantizeHSV(origImg: np.ndarray, k: int) -> np.ndarray:
         - quantizedImg: Quantized image with shape H x W x 3 and dtype "uint8"
         - clusterCenterHues: k x 1 vector of hue centers
     """
-    quantizedImg = np.zeros_like(origImg)
+    
     ######################################################################################
     ## TODO: YOUR CODE GOES HERE                                                        ##
     ######################################################################################
 
+    h, w = origImg.shape[0], origImg.shape[1]
+    hsv_img = rgb2hsv(origImg)
+    flat_img = hsv_img.reshape(h*w, 3)
+    quantizedImg = np.copy(flat_img)
+    hue_img = flat_img[:, 0].reshape(h*w, 1)
+    # print(hue_img)
+
+    km = KMeans(n_clusters=k, random_state=101)
+    km.fit_predict(hue_img)
+    clusterCenterHues = km.cluster_centers_
+    labels = km.labels_
+
+    for i in range(len(labels)):
+        quantizedImg[i, 0] = clusterCenterHues[labels[i]]
+
+    quantizedImg = np.floor(hsv2rgb(quantizedImg.reshape(h, w, 3))*255.0).astype(int)
+
+    return quantizedImg, clusterCenterHues
+
+
     ######################################################################################
     ## YOUR CODE ENDS HERE                                                              ##
     ######################################################################################
-    raise NotImplementedError
+    
 
 
 def computeQuantizationError(origImg: np.ndarray, quantizedImg: np.ndarray) -> int:
@@ -76,7 +110,9 @@ def computeQuantizationError(origImg: np.ndarray, quantizedImg: np.ndarray) -> i
     ## TODO: YOUR CODE GOES HERE                                                        ##
     ######################################################################################
 
+    quantizationError = np.sum(np.square(origImg - quantizedImg))
+
     ######################################################################################
     ## YOUR CODE ENDS HERE                                                              ##
     ######################################################################################
-    raise NotImplementedError
+    return quantizationError
